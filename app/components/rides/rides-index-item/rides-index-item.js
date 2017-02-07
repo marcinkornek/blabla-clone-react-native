648d70951636c2b7021008fe43b47d426d823be1
marcinkornek <marcinkornek@gmail.com>
2017-02-07 18:23:57 +0100

    Show different colors for driver and passenger rides

--- app/components/rides/rides-index-item/rides-index-item.js
+++ app/components/rides/rides-index-item/rides-index-item.js
@@ -20,10 +20,23 @@ export class RidesIndexItem extends Component {
     }
   }
 
-  render() {
+  renderRide() {
     const { ride } = this.props;
 
-    return(
+    switch (ride.user_role) {
+    case "driver":
+      return this.renderRideAsDriver()
+    case "passenger":
+      return this.renderRideAsPassenger()
+    default:
+      return this.renderNormalRide()
+    }
+  }
+
+  renderNormalRide() {
+    const { ride } = this.props;
+
+    return (
       <ListItem
         onPress={() => Actions.rideShow({rideId: ride.id})}
         key={ride.id}
@@ -32,5 +45,55 @@ export class RidesIndexItem extends Component {
         avatar={{uri: this.renderAvatar()}}
       />
     )
+  }
+
+  renderRideAsDriver() {
+    const { ride } = this.props;
+
+    return (
+      <ListItem
+        containerStyle={{backgroundColor: '#edf3fd'}}
+        onPress={() => Actions.rideShow({rideId: ride.id})}
+        key={ride.id}
+        title={`${ride.start_location.address} - ${ride.destination_location.address}`}
+        subtitle={`${moment(new Date(ride.start_date)).format('DD.MM.YY - H:mm')} - ${ride.price} ${ride.currency}`}
+        avatar={{uri: this.renderAvatar()}}
+      />
+    )
+  }
+
+  renderRideAsPassenger() {
+    const { ride } = this.props;
+    const color = this.rideAsPassengerColor()
+
+    return (
+      <ListItem
+        containerStyle={{backgroundColor: color}}
+        onPress={() => Actions.rideShow({rideId: ride.id})}
+        key={ride.id}
+        title={`${ride.start_location.address} - ${ride.destination_location.address}`}
+        subtitle={`${moment(new Date(ride.start_date)).format('DD.MM.YY - H:mm')} - ${ride.price} ${ride.currency}`}
+        avatar={{uri: this.renderAvatar()}}
+      />
+    )
+  }
+
+  rideAsPassengerColor() {
+    const { ride } = this.props;
+
+    switch (ride.user_ride_request_status) {
+    case "pending":
+      return '#fff0cc'
+    case "rejected":
+      return '#ffe5e5'
+    case "accepted":
+      return '#e6f4e5'
+    default:
+      return 'white'
+    }
+  }
+
+  render() {
+    return this.renderRide()
   }
 };

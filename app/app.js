ce2f99359cbac46d0205db6e417cedb91dcdd6ec
marcinkornek <marcinkornek@gmail.com>
2017-02-25 22:15:41 +0100

    Add test onesignal to app

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { persistStore } from 'redux-persist';
 import { AsyncStorage, Text } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 
 // components
 import RootRouter from './router'
@@ -14,11 +15,41 @@ class App extends Component {
     rehydrated: false
   }
 
-   componentWillMount(){
-      persistStore(store, { storage: AsyncStorage, whitelist: ['session', 'currentUser', 'rides', 'users'] }, () => {
-       this.setState({ rehydrated: true })
-     })
-   }
+  componentWillMount(){
+    persistStore(store, { storage: AsyncStorage, whitelist: ['session', 'currentUser', 'rides', 'users'] }, () => {
+      this.setState({ rehydrated: true })
+    })
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('registered', this.onRegistered);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('registered', this.onRegistered);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onRegistered(notifData) {
+    console.log("Device had been registered for push notifications!", notifData);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
 
   render() {
     if(!this.state.rehydrated){

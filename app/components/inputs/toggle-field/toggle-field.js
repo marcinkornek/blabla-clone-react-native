587a0452d16022a92720c21c596219665d168e8f
marcinkornek <marcinkornek@gmail.com>
2017-03-09 22:14:46 +0100

    Add ToggleInput component

---
+++ app/components/inputs/toggle-field/toggle-field.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Text, Switch, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  error: {
+    color: 'red',
+    marginLeft: 15,
+    marginTop: -5,
+    marginBottom: 5,
+  },
+  label: {
+    marginLeft: 15,
+  },
+  input: {
+    height: 40,
+    marginRight: 10,
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  }
+});
+
+export const ToggleField = ({ input, label, meta: { touched, error }, ...custom }) => (
+  <View>
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <Switch
+        style={styles.input}
+        value={input.value}
+        onValueChange={(value) => { input.onChange(value) }}
+        {...custom}
+      />
+    </View>
+    <Text style={styles.error}>{touched && error}</Text>
+  </View>
+);
